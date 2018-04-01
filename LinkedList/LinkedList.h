ba605635ef8d82fcee9c679298e44d96b9505978
amguinto <37889885+amguinto@users.noreply.github.com>
2018-04-01 18:28:18 -0400

    Progress. NOTE: Still not complete.

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,34 @@
+class LinkedList {
+
+protected:
+	//What all Nodes have.
+	struct Node {
+		int data;
+		Node* next;
+	};
+
+	Node* head;
+	Node* tail;
+	Node* current;
+public:
+	LinkedList();
+	~LinkedList();
+};
+
+class AddNode : LinkedList {
+public:
+	//Add Node to List.
+	void Add(int data);
+
+	AddNode();
+	~AddNode();
+};
+
+class DeleteNode : LinkedList {
+public:
+	//Delete Node from the list.
+	void Delete(int data);
+
+	DeleteNode();
+	~DeleteNode();
+};
---
+++ LinkedList/main.cpp
@@ -0,0 +1,18 @@
+#include "LinkedList.h"
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	//vector<int> list = { 1, 5, 2, 8, 3 };
+	LinkedList ll;
+
+	//Call AddNode Constructor.
+	AddNode a;
+	
+	a.Add(5);
+
+	return 0;
+}
